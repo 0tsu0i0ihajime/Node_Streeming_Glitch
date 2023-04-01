581a8361734c8e2e9e5fddb79661b4624a1bd1b6
0tsu0i0ihajime <110276910+0tsu0i0ihajime@users.noreply.github.com>
2023-04-01 12:48:48 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const qrcode = require('qrcode');
 const ytdl = require('ytdl-core');
 const path = require('path');
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/download', async(req, res) => {
+  const link = req.query.link;
+  const qr = req.query.qr;
+  
+  if(qr) {
+    const decoded = await qrcode.toString(qr, { type: 'terminal' });
+    
+    if (decoded === link) {
+      res.header('Content-Disposition', `attachment; filename="audio.mp3"`);
+      ytdl(link, { filter: 'audioonly' }).pipe(res);
+    } else {
+      res.send('Invalid QR code');
+    }
+  } else {
+    res.send('QR code not found');
+  }
 });
 
 app.get('/stream', async(req, res) => {
@@ -15,6 +34,6 @@ app.get('/stream', async(req, res) => {
     audioStream.pipe(res);
 });
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Server listening on port ${process.env.PORT || 8080}`);
-})
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
